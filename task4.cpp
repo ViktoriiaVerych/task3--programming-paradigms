83210dc51bb5f34d4cf9a94c62ffa559702fbbfa
viktoriia <113999567+ViktoriiaVerych@users.noreply.github.com>
2023-10-08 15:42:17 +0300

    Decryption algorithm | Milestone #2

--- task4.cpp
+++ task4.cpp
@@ -17,21 +17,40 @@ char* encrypt(char* rawText, int key) {
     return encryptedText;
 }
 
+char* decrypt(char* encryptedText, int key) {
+    int length = strlen(encryptedText);
+    char* decryptedText = new char[length + 1];
+    for (int i = 0; i < length; ++i) {
+        char ch = encryptedText[i];
+        if (isalpha(ch)) {
+            char base = islower(ch) ? 'a' : 'A';
+
+            decryptedText[i] = (ch - base - key + 26) % 26 + base;
+        } else {
+            decryptedText[i] = ch;
+        }
+    }
+    decryptedText[length] = '\0';
+    return decryptedText;
+}
+
 int main() {
     std::cout << "Enter the text to encrypt: ";
     std::string inputText;
     std::getline(std::cin, inputText);
 
     int key;
-    std::cout << "Enter the encryption key, int: ";
+    std::cout << "Enter the encryption key (an integer): ";
     std::cin >> key;
 
     char* encryptedText = encrypt(&inputText[0], key);
+    char* decryptedText = decrypt(encryptedText, key);
 
     std::cout << "Encrypted Text: " << encryptedText << std::endl;
+    std::cout << "Decrypted Text: " << decryptedText << std::endl;
 
     delete[] encryptedText;
+    delete[] decryptedText;
 
     return 0;
 }
-
